e0f699aedc4855a2e48eb3c4d4465e1ca3dcbae8
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-05 11:01:22 -0500

    Solucion Top, Decoracion Header Menu part II

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -28,46 +28,23 @@ const header = () => (
             id="navbarSupportedContent">
             <ul className="navbar-nav text-right">
               <li className="nav-item active">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/">
                   Home <span className="sr-only">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
+                  Formación
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false">
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div className="dropdown-divider" />
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="#"
-                  tabIndex="-1"
-                  aria-disabled="true">
-                  Disabled
+                <a className="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contáctanos
                 </a>
               </li>
             </ul>
