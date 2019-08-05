bf941d254406fba7e54b0c2d6848492c5aded76b
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-05 09:30:39 -0500

    Solucion Responsive Table en Top

--- src/layouts/top/top.jsx
+++ src/layouts/top/top.jsx
@@ -5,14 +5,14 @@ const top = () => (
   <div className={classes.Top}>
     <div className="container">
       <div className="row">
-        <div className="col-md-6 d-none d-sm-block">
+        <div className="col-md-6 d-none d-sm-block d-md-none d-lg-block">
           <div className="colGrid_2">
             <p>LLamalo: (+574) 322 23 23</p>
             <span>|</span>
             <p>Email: info@segurando.com</p>
           </div>
         </div>
-        <div className="col-md-6 col-sm-12">
+        <div className="col-lg-6 col-sm-12">
           <div className="colGrid_2 d-flex justify-content-end">
             <a href="/help">
               <box-icon name="help-circle" />
