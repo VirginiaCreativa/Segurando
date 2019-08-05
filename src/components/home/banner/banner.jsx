909006cc5a42644908c91a0e25c7f178547580ce
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-05 18:28:29 -0500

    Decoracion Banner content part II

--- src/components/home/banner/banner.jsx
+++ src/components/home/banner/banner.jsx
@@ -4,9 +4,22 @@ import classes from './banner.module.scss';
 const banner = () => (
   <div className={classes.Banner}>
     <div className={classes.bgOverly}>
-      <div className={classes.Content}>
-        <div className="container">
-          <h3>Titulo</h3>
+      <div className="container">
+        <div className={classes.Content}>
+          <div className={classes.LineGreen} />
+          <div className={classes.Heading}>
+            <h2>OFRECE CURSOS PARA</h2>
+            <h1>VENDEDORES ESTRELLAS, SE CAPACITAN Y MEJORES VENDENDORES</h1>
+          </div>
+          <div className={classes.Parrag}>
+            <p>
+              Los vendedores puede escoger entre varios curso. Quieres saberlo
+              más.
+            </p>
+          </div>
+          <button type="button" className="btn btn-primary">
+            contáctamos!
+          </button>
         </div>
       </div>
     </div>
