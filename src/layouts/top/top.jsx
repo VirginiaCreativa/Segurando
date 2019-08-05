646d9a6b55ecf71be55c13383c0c7cd594a3a25b
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-05 09:23:10 -0500

    Decoración Top

--- src/layouts/top/top.jsx
+++ src/layouts/top/top.jsx
@@ -3,7 +3,43 @@ import classes from './top.module.scss';
 
 const top = () => (
   <div className={classes.Top}>
-    <div className="container" />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 d-none d-sm-block">
+          <div className="colGrid_2">
+            <p>LLamalo: (+574) 322 23 23</p>
+            <span>|</span>
+            <p>Email: info@segurando.com</p>
+          </div>
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <div className="colGrid_2 d-flex justify-content-end">
+            <a href="/help">
+              <box-icon name="help-circle" />
+              Tienes duda
+            </a>
+            <span>|</span>
+            <ul className="list-unstyled list-inline">
+              <li className="list-inline-item">
+                <box-icon type="logo" name="facebook" />
+              </li>
+              <li className="list-inline-item">
+                <box-icon type="logo" name="twitter" />
+              </li>
+              <li className="list-inline-item">
+                <box-icon type="logo" name="youtube" />
+              </li>
+              <li className="list-inline-item">
+                <box-icon type="logo" name="instagram" />
+              </li>
+              <li className="list-inline-item">
+                <box-icon type="logo" name="linkedin" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
