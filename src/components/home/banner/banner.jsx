4bf45c5a889891224a908fe2c3ffadc6016f7cfe
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-05 18:18:28 -0500

    Decoracion Banner content part I

--- src/components/home/banner/banner.jsx
+++ src/components/home/banner/banner.jsx
@@ -3,7 +3,13 @@ import classes from './banner.module.scss';
 
 const banner = () => (
   <div className={classes.Banner}>
-    <div className={classes.bgOverly} />
+    <div className={classes.bgOverly}>
+      <div className={classes.Content}>
+        <div className="container">
+          <h3>Titulo</h3>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
