14dc810f69bf4f88993638b94c748cbc1f9180e2
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-07 11:13:49 -0500

    Decoracion Footer part II

--- src/layouts/footer/footer.jsx
+++ src/layouts/footer/footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import classes from './footer.module.scss';
 
-const footer = () => (
-  <div className={classes.Footer}>
-    <h1>Footer</h1>
-  </div>
-);
+const footer = () => {
+  const year = 2019;
+  return (
+    <div className={classes.Footer}>
+      <div className={classes.Top}>
+        <div className="container">
+          <div className="row">
+            <div className="col">logo</div>
+            <div className="col">categoria</div>
+            <div className="col">Direccion</div>
+            <div className="col">Busccador</div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.Bottom}>
+        <hr />
+        <div className="container">
+          <p>©{year} By: Tech and Solve, Medellin</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default footer;
