76f0a40d157d54c83d6304bf1605034178aa56c1
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-07 10:32:02 -0500

    Decoracion Footer part I

---
+++ src/layouts/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './footer.module.scss';
+
+const footer = () => (
+  <div className={classes.Footer}>
+    <h1>Footer</h1>
+  </div>
+);
+
+export default footer;
--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import classes from './layout.module.scss';
 
 import Top from './top/top';
 import Header from './header/header';
+import Footer from './footer/footer';
 
 const layout = ({ children }) => (
-  <>
+  <div className={classes.Layout}>
     <Top />
     <Header />
     {children}
-  </>
+    <Footer />
+  </div>
 );
 
 export default layout;
