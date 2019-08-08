eff14756cab19d408f175c420190336d73d588bc
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-08-07 19:40:26 -0500

    Arreglando Logo ponga enlace para home

--- src/layouts/logo/logo.jsx
+++ src/layouts/logo/logo.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import { Link } from 'react-router-dom';
 
 import BlackLogo from '../../assets/logo/19_01_UX_SG_Logo_Black_V1.svg';
 import WhiteLogo from '../../assets/logo/19_01_UX_SG_Logo_White_V1.svg';
@@ -8,11 +9,13 @@ import classes from './logo.module.scss';
 
 const logo = ({ selectsColor }) => (
   <>
-    {selectsColor === 1 ? (
-      <img src={BlackLogo} alt="Logo Segurando" className={classes.Logo} />
-    ) : (
-      <img src={WhiteLogo} alt="Logo Segurando" className={classes.Logo} />
-    )}
+    <Link to="/">
+      {selectsColor === 1 ? (
+        <img src={BlackLogo} alt="Logo Segurando" className={classes.Logo} />
+      ) : (
+        <img src={WhiteLogo} alt="Logo Segurando" className={classes.Logo} />
+      )}
+    </Link>
   </>
 );
 
